d2fddcb5ebb742db3242700ea077b83428eb24a0
Alexander Eichhorn <a.eichhorn@losjet.com>
2019-11-16 21:52:58 +0100

    added structure and basic tests

--- Sources/VivoExtractor/VivoExtractor.swift
+++ Sources/VivoExtractor/VivoExtractor.swift
@@ -1,3 +1,13 @@
-struct VivoExtractor {
-    var text = "Hello, World!"
+import Foundation
+
+public class VivoExtractor {
+    
+    public class func extract(fromHTML html: String) -> URL? {
+        return nil
+    }
+    
+    public class func extract(fromURL url: URL, completion: (URL?) -> Void) {
+        completion(nil)
+    }
+    
 }
--- Tests/VivoExtractorTests/VivoExtractorTests.swift
+++ Tests/VivoExtractorTests/VivoExtractorTests.swift
@@ -2,14 +2,41 @@ import XCTest
 @testable import VivoExtractor
 
 final class VivoExtractorTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(VivoExtractor().text, "Hello, World!")
+    
+    func testVideoURL(_ videoURL: URL) -> URL? {
+        
+        let expectation = self.expectation(description: "extraction")
+        var url: URL?
+        
+        VivoExtractor.extract(fromURL: videoURL) { sourceURL in
+            url = sourceURL
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 10.0, handler: nil)
+        
+        return url
+    }
+    
+    func testBunnyVideo() {
+        let url = testVideoURL(URL(string: "https://vivo.sx/b36ee9e324")!)
+        
+        XCTAssertNotNil(url)
+    }
+    
+    func testEmptyHTML() {
+        let url = VivoExtractor.extract(fromHTML: "")
+        XCTAssertNil(url)
+    }
+    
+    func testUnavailableURL() {
+        let url = testVideoURL(URL(string: "https://vivo.sx/abcdefghij")!)
+        XCTAssertNil(url)
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testEmptyHTML", testEmptyHTML),
+        ("testUnavailableURL", testUnavailableURL),
+        ("testBunnyVideo", testBunnyVideo),
     ]
 }

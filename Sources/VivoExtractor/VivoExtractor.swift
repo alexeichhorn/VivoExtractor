8e0a1fe5223bcf7137da0d071a8495b797633ddb
Alexander Eichhorn <a.eichhorn@losjet.com>
2019-11-17 02:17:35 +0100

    added documentation

--- Sources/VivoExtractor/VivoExtractor.swift
+++ Sources/VivoExtractor/VivoExtractor.swift
@@ -5,6 +5,9 @@ import FoundationNetworking
 
 public class VivoExtractor {
     
+    /// decodes direct video url from encoded source string from vivo page
+    /// - parameter encodedSource: encoded url from vivo
+    /// - returns: video url when found
     public class func extract(fromEncodedSource encodedSource: String) -> URL? {
         var decoded = ""
         for c in encodedSource.removingPercentEncoding ?? "" {
@@ -18,6 +21,10 @@ public class VivoExtractor {
         return URL(string: decoded)
     }
     
+    
+    /// extracts direct video url from raw html of vivo page
+    /// - parameter html: HTML of video page on vivo.sx
+    /// - returns: video url when found
     public class func extract(fromHTML html: String) -> URL? {
         
         let pattern = #"InitializeStream\s*\(\{[\s\S]*(source\:[\s]\')(?<url>[\s\S\:]*?)(\',\s*)"#
@@ -33,6 +40,10 @@ public class VivoExtractor {
         return extract(fromEncodedSource: encodedSource)
     }
     
+    
+    /// extracts direct video url from standard vivo url
+    /// - parameter url: vivo url (e.g.: https://vivo.sx/b36ee9e324)
+    /// - parameter completion: called when result is found. returns video url
     public class func extract(fromURL url: URL, completion: @escaping (URL?) -> Void) {
         
         #if os(Linux)
